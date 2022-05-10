45cafadee7851b319f542d60a8aec4f2a233460d
ing-arriola <ingeniero.arriola@gmail.com>
2022-05-09 19:20:34 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state={
+    busqueda:"",
+    resultados:[],
+    loading:true
+  }
+  // Funcion que se ejecuta cuando se hace click en el boton
+  handleSearch = async () => {
+    
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${this.state.busqueda}&limit=10&rating=G`)
+    console.log(res.data.data)
+    this.setState({resultados:res.data.data, loading:false})
+
+  }
+
+
+  render() {
+    return (
+      <>
+        <input 
+          value={this.state.busqueda}  
+          onChange={ (e) => this.setState({busqueda:e.target.value}) }
+          type="text" 
+          placeholder='Ingresa tu busqueda' />
+        <button onClick={()=>this.handleSearch()} >Buscar</button>
+        { !this.state.loading && this.state.resultados.map( imagen => (
+          <img src={imagen.images.downsized.url} key={imagen.id} alt='imagen' />
+        ) )}
+        
+      </>
+    )
+  }
 }
-
-export default App;
