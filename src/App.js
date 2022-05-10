254abf9d6538315347325745570ddf8377e87d9d
ing-arriola <ingeniero.arriola@gmail.com>
2022-05-09 20:17:36 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import axios from 'axios'
 import React, { Component } from 'react'
+import SearchBar from '../src/components/SearchBar'
 
 export default class App extends Component {
   state={
-    busqueda:"",
     resultados:[],
     loading:true
   }
   // Funcion que se ejecuta cuando se hace click en el boton
-  handleSearch = async () => {
-    
-    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${this.state.busqueda}&limit=10&rating=G`)
+  handleSearch = async ( busqueda )  => {
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${busqueda}&limit=10&rating=G`)
     console.log(res.data.data)
     this.setState({resultados:res.data.data, loading:false})
 
   }
 
+/*   componentWillMount(){
+    console.log('Componente a montarse')
+  }
+
+  componentDidMount(){
+    console.log('Componente montado')
+  } */
+
 
   render() {
     return (
       <>
-        <input 
-          value={this.state.busqueda}  
-          onChange={ (e) => this.setState({busqueda:e.target.value}) }
-          type="text" 
-          placeholder='Ingresa tu busqueda' />
-        <button onClick={()=>this.handleSearch()} >Buscar</button>
+        <SearchBar 
+          manejarBusqueda={this.handleSearch}
+          />
         { !this.state.loading && this.state.resultados.map( imagen => (
           <img src={imagen.images.downsized.url} key={imagen.id} alt='imagen' />
         ) )}
